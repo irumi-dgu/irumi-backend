85b6bb17275e29ede97eb6bab9eeefcde02b5fa3
DIDICHAA <jenny615@dongguk.edu>
2023-09-19 00:55:42 +0900

    :sparkles: Lantern model 확인용

--- lantern/models.py
+++ lantern/models.py
@@ -4,9 +4,9 @@ class Lantern(models.Model):
     id = models.AutoField(primary_key=True)
     nickname = models.CharField(max_length=10, null=False, blank=False)
     content = models.TextField(max_length=100, null=False, blank=False)
-    #password = models.CharField(
-    #    max_length=4, 
-    #    min_length=4, 
-    #    validators=[RegexValidator(r'^\d{4}$', "네 자리 숫자를 입력해주세요.")], 
-    #    help_text="네 자리 숫자를 입력해주세요."
-    #)
+    password = models.CharField(
+        max_length=4, 
+        min_length=4, 
+        validators=[RegexValidator(r'^\d{4}$', "네 자리 숫자를 입력해주세요.")], 
+        help_text="네 자리 숫자를 입력해주세요."
+    )
