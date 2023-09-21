e03edf155ffde197a38e7ccd15eddb0e5c7a683d
Sangjun Lee <112681633+lsjmc0224@users.noreply.github.com>
2023-09-21 22:46:44 +0900

    :art: Edited lantern/urls.py
    랜턴 url을 디폴트 라우터에서 심플라우터로 바꿨습니다.

--- lantern/urls.py
+++ lantern/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import LanternViewSet
 
-router = DefaultRouter(trailing_slash=False)
-router.register(r'lanterns', LanternViewSet)
+default_router = routers.SimpleRouter(trailing_slash=False)
+default_router.register("lanterns", LanternViewSet, basename="lanterns")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(default_router.urls)),
 ]
