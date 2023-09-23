abd5a2b9cfba091aa379334b735ad16bed610f43
Sangjun Lee <lsjmc0224@dgu.edu>
2023-09-23 20:03:13 +0900

    :sparkles: Added lantern color field in lantern model
    랜턴 모델에 랜턴 색 integer+choice 필드로 추가하였습니다.

--- lantern/models.py
+++ lantern/models.py
@@ -3,6 +3,7 @@ from django.core.validators import RegexValidator
 from django.utils.crypto import get_random_string
 
 class Lantern(models.Model):
+
     id = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     nickname = models.CharField(
@@ -11,7 +12,7 @@ class Lantern(models.Model):
         blank=False,
         validators=[RegexValidator(r'^[^\s]{1,10}$', "공백없이 닉네임을 입력해주세요.")],
         help_text="공백없이 닉네임을 입력해주세요."
-    )    
+    )
     content = models.TextField(max_length=100, null=False, blank=False)
     password = models.CharField(
         max_length=4, 
@@ -21,6 +22,14 @@ class Lantern(models.Model):
     light_bool = models.BooleanField(default=False)
     is_liked = models.BooleanField(default=False)
     is_reported = models.BooleanField(default=False)
+    COLOR_CHOICES = [
+        (1, '1'), # red
+        (2, '2'), # yellow
+        (3, '3'), # green
+        (4, '4'), # blue
+        (5, '5'), # purple
+    ]
+    lantern_color = models.IntegerField(choices=COLOR_CHOICES, default=1)
 
 class LanternReaction(models.Model):
     REACTION_CHOICES = (("like", "Like"), ("dislike", "Dislike"))
