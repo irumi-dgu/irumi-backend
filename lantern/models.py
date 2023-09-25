2dbe675c30cdb007d4c47c58b6d6ab8a48adc56d
DIDICHAA <jenny615@dongguk.edu>
2023-09-25 16:27:09 +0900

    :fire: Remove key

--- lantern/models.py
+++ lantern/models.py
@@ -54,4 +54,4 @@ class Report(models.Model):
     lantern = models.ForeignKey(Lantern, on_delete=models.CASCADE)
     categories = models.ManyToManyField(ReportCategory)
     created_at = models.DateTimeField(auto_now_add=True)
-    key = models.CharField(max_length=10, default=get_random_string(length=10))
+    #key = models.CharField(max_length=10, default=get_random_string(length=10))
