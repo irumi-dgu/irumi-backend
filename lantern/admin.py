a991dd8062d3dd46bc3ae41cf960ec639e319772
DIDICHAA <jenny615@dongguk.edu>
2023-10-06 02:23:24 +0900

    :ambulance: Fixed admin test 03

--- lantern/admin.py
+++ lantern/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 
+#for test
 class ReportAdmin(admin.ModelAdmin):
     list_display = ('id', 'lantern_content', 'reported_categories', 'user_id', 'created_at')
     list_filter = ('created_at', )
