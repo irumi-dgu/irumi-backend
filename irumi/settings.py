ac66c94958393616e9f576b2e8df3865b6a2593d
DIDICHAA <jenny615@dongguk.edu>
2023-10-05 23:48:33 +0900

    :fire: Remove https

--- irumi/settings.py
+++ irumi/settings.py
@@ -172,13 +172,4 @@ CORS_ALLOWED_ORIGINS = [
     "http://52.78.201.149",
     "http://52.78.201.149:5173",
     "http://54.180.167.23",
-
-    "https://127.0.0.1:8000",
-    "https://localhost:8000",
-    "https://127.0.0.1:5173",
-    "https://localhost:5173",
-    "https://iirumi.com",
-    "https://52.78.201.149",
-    "https://52.78.201.149:5173",
-    "https://54.180.167.23"
 ]
